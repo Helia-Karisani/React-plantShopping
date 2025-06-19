60f9a75f75eea07b66e239b073ca123735039359
Helia_Karisani <helia.karisani@gmail.com>
2025-06-19 19:55:24 -0400

    add handleDecrement and handleIncrement

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -26,11 +26,15 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleIncrement = (item) => {
-
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
